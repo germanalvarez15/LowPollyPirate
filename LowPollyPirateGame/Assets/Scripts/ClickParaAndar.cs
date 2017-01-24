a99aef8d18f4cf5007471ff67f92bf0d9f91521d
germanalvarez15 <germanalvarez15@gmail.com>
2017-01-24 00:41:16 -0300

    Movimiento funciona cuando se clickea sobre el plano pero el cubo no se mueve hacia donde deberia. Se mueve unicamente a la derecha-abajo.

---
+++ LowPollyPirateGame/Assets/Scripts/ClickParaAndar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickParaAndar : MonoBehaviour {
+
+	// Use this for initialization
+	public Vector3 posicionInicial;
+	bool inicio = true;
+
+	void Start () {
+		this.transform.position = posicionInicial;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if(Physics.Raycast(ray, out hit)){
+				if(hit.rigidbody != null){
+					MovimientoContinuo.posicionALaQueSeMovera = hit.point;
+
+				}
+
+			}
+		}
+	}
+}
---
+++ LowPollyPirateGame/Assets/Scripts/MovimientoContinuo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoContinuo : MonoBehaviour {
+
+	// Use this for initialization
+	public static Vector3 posicionALaQueSeMovera;
+	public float velocidad = 3f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float pasos = velocidad * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, posicionALaQueSeMovera, pasos);
+		
+	}
+}
